b2a8ef1973890a28cecd2f69676e70ef5f599daa
awni rifai <awni.rifai1998@gmail.com>
2021-12-23 20:38:57 +0300

    first

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,57 @@
+<body>
+<div class="left-sidebar-pro">
+    <nav id="sidebar" class="">
+        <div class="sidebar-header">
+            <a href="index.html"><img class="main-logo" src="{{asset('admin_styles/img/logo/logo.png')}}" alt="" /></a>
+            <strong><a href="index.html"><img src="{{asset('admin_styles/img/logo/logosn.png')}}" alt="" /></a></strong>
+        </div>
+        <div class="left-custom-menu-adp-wrap comment-scrollbar">
+            <nav class="sidebar-nav left-sidebar-menu-pro">
+                <ul class="metismenu" id="menu1">
+                    <li class="active">
+                        <a class="has-arrow" href="index.html">
+                            <span class="educate-icon educate-home icon-wrap"></span>
+                            <span class="mini-click-non">Dashboard</span>
+                        </a>
+                        <ul class="submenu-angle" aria-expanded="true">
+
+                            <li><a title="Dashboard v.2" href="index.html"><span class="mini-sub-pro">Dashboard v.2</span></a></li>
+
+                            <li><a title="Analytics" href="analytics.html"><span class="mini-sub-pro">Analytics</span></a></li>
+
+                        </ul>
+                    </li>
+                    <li>
+                        <a title="Landing Page" href="events.html" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Event</span></a>
+                    </li>
+                    <li>
+                        <a  href="all-professors.html" aria-expanded="false"><span class="educate-icon educate-professor icon-wrap"></span> <span class="mini-click-non">Rooms</span></a>
+
+                    </li>
+                    <li>
+                        <a  href="all-students.html" aria-expanded="false"><span class="educate-icon educate-student icon-wrap"></span> <span class="mini-click-non">Categories</span></a>
+
+                    </li>
+                    <li>
+                        <a  href="all-courses.html" aria-expanded="false"><span class="educate-icon educate-course icon-wrap"></span> <span class="mini-click-non">Reservation</span></a>
+
+                    </li>
+                    <li>
+                        <a  href="all-courses.html" aria-expanded="false"><span class="educate-icon educate-library icon-wrap"></span> <span class="mini-click-non">Meals</span></a>
+
+                    </li>
+                    <li>
+                        <a  href="all-courses.html" aria-expanded="false"><span class="educate-icon educate-department icon-wrap"></span> <span class="mini-click-non">Users</span></a>
+
+                    </li>
+                    <li>
+                        <a  href="mailbox.html" aria-expanded="false"><span class="educate-icon educate-message icon-wrap"></span> <span class="mini-click-non">Reviews</span></a>
+
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    </nav>
+</div>
+<!-- End Header menu area -->
