a042b70a0dab2978d98c9f082e2697c0e1bb8024
awni rifai <awni.rifai1998@gmail.com>
2021-12-24 15:13:48 +0300

    added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.index-2');
+    return view('pages.index');
 });
 Route::get('/admin', function () {
     return view('admin.index');
 });
-
+Route::get('/test',[RoomController::class,'test']);
